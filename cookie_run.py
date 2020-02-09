60a2b4b2881f369714a2e215f798d90233ec563b
Thurston Zhu <thurstonwzhu@gmail.com>
2020-02-08 17:05:03 -0800

    Added some cookies and probabilitys

--- cookie_run.py
+++ cookie_run.py
@@ -13,4 +13,19 @@ lst = [
 for cool_num, something_else in enumerate(lst):
     print(f"{cool_num} the saying of the day is \"{something_else}\"")
 
+print()
 
+cookies = [
+    ["Moonlight Cookie", 1],
+    ["Pitaya Dragon Cookie", 1],
+    ["Wind Archer Cookie", 10],
+    ["Sea Fairy Cookie", 5],
+    ["Millenial Tree Cookie", 10],
+    ["Dark Enchantress Cookie", 70],
+    ["Fire Spirit Cookie", 3],
+]
+
+for cookie, probability in cookies:
+    print(f"This cookie is named {cookie}")
+    print(f"The probability of getting {cookie} is {probability}%")
+    print()
